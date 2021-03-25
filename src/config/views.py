57a395decc09a4a11ae669bb0cf965fc4d24a282
hinagawa <psalimullina@gmail.com>
2021-03-25 21:02:47 +0300

    Strategy pattern

--- src/config/views.py
+++ src/config/views.py
@@ -1,16 +1,61 @@
-import json
+from abc import ABC
 
 from django.http import JsonResponse, HttpResponse
 from server.settings import DATABASE
 
 
+class ConfigStrategy(ABC):
+    def __init__(self):
+        self._config = None
+
+    @property
+    def config(self):
+        return self._config
+
+    @config.setter
+    def config(self, config) -> None:
+        self._config = config
+
+
+class AuthConfigStrategy(ConfigStrategy):
+    @property
+    def config(self):
+        return {"config": self._config}
+
+    @config.setter
+    def config(self, config) -> None:
+        self._config = config
+
+
+class DataConfigStrategy(ConfigStrategy):
+    @property
+    def config(self):
+        return {
+            "url": f"postgresql+psycopg2://{self._config['USER']}:{self._config['PASSWORD']}@"
+            f"{self._config['HOST']}:{self._config['PORT']}/{self._config['NAME']}"
+        }
+
+    @config.setter
+    def config(self, config) -> None:
+        self._config = config
+
+
+def choose_strategy(operation):
+    if operation == "data":
+        strategy = DataConfigStrategy()
+    elif operation == "auth":
+        strategy = AuthConfigStrategy()
+    else:
+        raise NotImplementedError
+    strategy.config = DATABASE
+    return strategy
+
+
 def get_config(request, operation):
-    config = DATABASE
+    strategy = choose_strategy(operation)
     if request.method == "GET":
-        if operation == "data":
-            url = f"postgresql+psycopg2://{config['USER']}:{config['PASSWORD']}@" \
-                  f"{config['HOST']}:{config['PORT']}/{config['NAME']}"
-            return JsonResponse(data={"url": url})
-        elif operation == "auth":
-            return JsonResponse(data={"config": config})
-        return HttpRespone(status=404)
+        try:
+            return JsonResponse(data=strategy.config)
+        except NotImplementedError:
+            return HttpResponse(status=501)
+    return HttpResponse(status=400)
